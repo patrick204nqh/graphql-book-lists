f86821ccd75e2ade8b816027e830341e489d8190
patrick <patrick204nqh@gmail.com>
2020-05-27 23:53:51 +0700

    composing queries

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import { getAuthorsQuery } from '../queries/queries';
+import { getAuthorsQuery, addBookMutation } from '../queries/queries';
+import * as compose from 'lodash.flowright';
 
 class AddBook extends Component {
   constructor(props) {
@@ -12,7 +13,8 @@ class AddBook extends Component {
     };
   }
   displayAuthors() {
-    var data = this.props.data;
+    var data = this.props.getAuthorsQuery;
+    console.log(data);
     if (data.loading) {
       return <option disabled>Loading Authors...</option>;
     } else {
@@ -27,7 +29,7 @@ class AddBook extends Component {
   }
   submitForm(e) {
     e.preventDefault();
-    console.log(this.state);
+    this.props.addBookMutation();
   }
 
   render() {
@@ -60,4 +62,7 @@ class AddBook extends Component {
   }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: 'getAuthorsQuery' }),
+  graphql(addBookMutation, { name: 'addBookMutation' })
+)(AddBook);
